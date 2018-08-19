774a99567b335acafd40a0acef42bcb98d74e1c0
Jasminenabila <jasminenabila19@gmail.com>
2018-08-19 16:10:56 +0700

    first commit

---
+++ app/src/main/java/com/example/universitasindonesia/kamus/Preference/KamusPreference.java
@@ -0,0 +1,31 @@
+package com.example.universitasindonesia.kamus.Preference;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+import com.example.universitasindonesia.kamus.R;
+
+public class KamusPreference {
+    SharedPreferences prefs;
+    Context context;
+
+    public KamusPreference(Context context) {
+        prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        this.context = context;
+    }
+
+    public void setFirstRun(Boolean input){
+
+        SharedPreferences.Editor editor = prefs.edit();
+        String key = context.getResources().getString(R.string.kamus_pref);
+        editor.putBoolean(key,input);
+        editor.commit();
+    }
+
+    public Boolean getFirstRun(){
+        String key = context.getResources().getString(R.string.kamus_pref);
+        return prefs.getBoolean(key, true);
+    }
+
+}
---
+++ app/src/main/java/com/example/universitasindonesia/kamus/ViewHolder/SearchViewHolder.java
@@ -0,0 +1,40 @@
+package com.example.universitasindonesia.kamus.ViewHolder;
+
+import android.content.Intent;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.universitasindonesia.kamus.DetailKamusActivity;
+import com.example.universitasindonesia.kamus.Model.KamusModel;
+import com.example.universitasindonesia.kamus.R;
+
+public class SearchViewHolder extends RecyclerView.ViewHolder {
+
+    TextView tvKosakata, tvArti, tvCategory;
+
+    public SearchViewHolder(View itemView) {
+        super(itemView);
+
+        tvKosakata  = (TextView)itemView.findViewById(R.id.tvKosakata);
+        tvArti      = (TextView)itemView.findViewById(R.id.tvArti);
+//        tvCategory  = (TextView)itemView.findViewById(R.id.tvCategory);
+    }
+
+    public void bind(final KamusModel kamusModel) {
+        tvKosakata.setText(kamusModel.getKata());
+        tvArti.setText(kamusModel.getDeskripsi());
+
+        itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(itemView.getContext(), DetailKamusActivity.class);
+                intent.putExtra(DetailKamusActivity.ITEM_KOSAKATA, kamusModel.getKata());
+                intent.putExtra(DetailKamusActivity.ITEM_ARTI, kamusModel.getDeskripsi());
+                intent.putExtra(DetailKamusActivity.ITEM_CATEGORY, kamusModel.getCategory());
+                itemView.getContext().startActivity(intent);
+            }
+        });
+    }
+
+}
